11b59c10c2cd9d9895fd272c802569758cb83ff5
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-03 11:38:03 +0330

    add confg sercrectkey

--- terminalconfig/settings.py
+++ terminalconfig/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+# from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-wq2y47d9e=yx&s_wyn$3bok2j2(wl7o&vjk5#tft&bxfqer_p4'
+# SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = config('DEBUG', cast=bool, default=False)
+
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
