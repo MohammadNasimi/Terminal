0becb651142d57ee32fc7a3ed5d2f75022713815
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-03 14:10:50 +0330

    add to admin terminal models

--- terminal/admin.py
+++ terminal/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from terminal.models import Route,Bus,Ticket
 # Register your models here.
+admin.site.register(Route)
+admin.site.register(Bus)
+admin.site.register(Ticket)
