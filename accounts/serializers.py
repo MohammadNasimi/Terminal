64bb8cd922eed95c18d8b3f22d5e456026cd3663
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-03 15:04:23 +0330

    ser Driver and Passenger

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,9 +1,22 @@
+from ast import Pass
 from rest_framework import serializers
-from accounts.models import User
+from accounts.models import User,Driver,Passenger
 class LoginSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'phone', 'password','email', 'first_name', 'last_name')
+        fields = ('id', 'phone', 'password','email', 'first_name', 'last_name','type')
         read_only_fields = ('id','email', 'first_name', 'last_name')
+        
+class Driverserializer(serializers.ModelSerializer):
+    user = LoginSerializer()
+    class Meta:
+        model = Driver
+        fields = ('user','Certificatenumber')
+
+class Passengerserializer(serializers.ModelSerializer):
+    user = LoginSerializer()
+    class Meta:
+        model = Passenger    
+        fields = ('user','nationalcode','birthday','accountbalance')
