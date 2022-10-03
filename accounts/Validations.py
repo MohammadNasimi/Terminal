9d12cde1f4582f7ad887a33e0129b156d7b844cb
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-03 13:17:52 +0330

    add validators birthday

--- accounts/Validations.py
+++ accounts/Validations.py
@@ -1,4 +1,6 @@
 from django.core.exceptions import ValidationError
+import datetime
+
 
 def validate_phone(value : str) :
     if  not value.startswith("09") :
@@ -9,5 +11,17 @@ def validate_phone(value : str) :
     if  len(value)!=11:
         raise ValidationError(
             ('size equal 11 example: 09141231213'),
+            params={'value': value},
+        )
+        
+def validate_birthday(value):
+    if  datetime.datetime.now().year - value.year > 15:
+        raise ValidationError(
+            ('you should older than 15 '),
+            params={'value': value},
+        )
+    if  datetime.datetime.now().year < value.year :
+        raise ValidationError(
+            ('less today time'),
             params={'value': value},
         )
