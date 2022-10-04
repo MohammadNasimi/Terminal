0a6368219fb5b429e5e324bb4b5480a217f0cdba
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-04 23:09:48 +0330

    CreateBusView add

--- terminal/views.py
+++ terminal/views.py
@@ -2,15 +2,17 @@
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 #serializer
-from terminal.serializers import Routeserializer
+from terminal.serializers import Routeserializer,Ticketserializer,Busserializer
 #models
-from terminal.models import Route
-from accounts.models import User,Manager
+from terminal.models import Route,Bus
+from accounts.models import Manager,Driver
 #rest framework
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 # permistions
-from terminal.permissions import IsOwnerOrReadOnlyRoute
-
+from terminal.permissions import IsOwnerOrReadOnlyRoute,IsOwnerOrReadOnlyBus
+# other app import
+from datetime import date
+###########ROUTE############################
 class CreateRouteView(ListCreateAPIView):
     serializer_class = Routeserializer
     permission_classes =[IsAuthenticated,IsOwnerOrReadOnlyRoute]
@@ -32,5 +34,24 @@ class CreateRouteView(ListCreateAPIView):
         serializer.save(manager_id = manager.id)  
         
     
-     
+##############BUS##########################################
+class CreateBusView(ListCreateAPIView):
+    serializer_class = Busserializer
+    permission_classes =[IsAuthenticated,IsOwnerOrReadOnlyBus]
     
+    def get_queryset(self):
+        queryset =Bus.objects.all()
+        codebus = self.request.GET.get('codebus')
+        if codebus is not None:
+            queryset=queryset.filter(codebus= codebus)
+        return queryset
+      
+    def perform_create(self, serializer):
+        driver = Driver.objects.get(user_id = self.request.user.id)
+        serializer.save(driver_id = driver.id)
+        
+        
+        # date = self.request.GET.get('date')
+        # if date is not None:
+        #     queryset=queryset.filter(date=date.fromisoformat(date))
+        # return queryset
