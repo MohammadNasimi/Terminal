c43780586221162fb256f61ea01b98c85f5a90be
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-04 23:08:53 +0330

    add BusRouteserializer

--- terminal/serializers.py
+++ terminal/serializers.py
@@ -1,18 +1,28 @@
 from rest_framework import serializers
 from terminal.models import Route,Bus,Ticket
 #ser accounts model 
-from accounts.serializers import RouteSerializer
+from accounts.serializers import ManagerSerializer,Driverserializer
 class Routeserializer(serializers.ModelSerializer):
-    manager = RouteSerializer(read_only =True)
+    manager = ManagerSerializer(read_only =True)
     class Meta:
         model = Route
         fields = ('id', 'manager', 'begin','destination','numberstation', 'distance', 'timeroute')
         read_only_fields = ['id']
+
 class Busserializer(serializers.ModelSerializer):
+    driver = Driverserializer(read_only =True)
     class Meta:
         model = Bus
-        fields = ('id', 'driver', 'route','codebus','usebus', 'productionyear', 'capacity'
-                  ,'date','hourmove')
+        fields = ('id', 'driver','codebus','usebus', 'productionyear', 'capacity')
+        read_only_fields = ['id']
+
+class BusRouteserializer(serializers.ModelSerializer):
+    driver = Driverserializer(read_only =True)
+    class Meta:
+        model = Bus
+        fields = ('id', 'route','bus','date','hourmove','capacity')
+        read_only_fields = ['id']
+        
 
 class Ticketserializer(serializers.ModelSerializer):
     class Meta:
