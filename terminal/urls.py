b4bf5ea82ba62cf25eec9922370653c26ccd7c25
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-04 23:09:10 +0330

    add bus list url

--- terminal/urls.py
+++ terminal/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
-from terminal.views import CreateRouteView
+from terminal.views import CreateRouteView,CreateBusView
 urlpatterns = [
     path('route/list/', CreateRouteView.as_view(), name='list_route'),
+    path('bus/list/', CreateBusView.as_view(), name='list_bus'),
 ]
