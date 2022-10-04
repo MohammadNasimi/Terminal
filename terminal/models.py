2270867c0c626a8179a9fafb9dce14faabdc4a96
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-04 23:08:35 +0330

    add bus and busroute models

--- terminal/models.py
+++ terminal/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-#accounts
-from accounts.models import Manager,Driver,Passenger
+# accounts
+from accounts.models import Manager,Passenger,Driver
 # Create your models here.
 class Route(models.Model):
     manager = models.ForeignKey(Manager,on_delete=models.CASCADE)
@@ -15,32 +15,49 @@ class Route(models.Model):
 
 class Bus(models.Model):
     driver = models.OneToOneField(Driver,on_delete=models.CASCADE)
-    route = models.ForeignKey(Route,on_delete=models.CASCADE)
-    codebus = models.CharField(max_length = 20) 
+    codebus = models.CharField(max_length = 20,null =False,blank=False) 
     usebus = models.CharField(max_length = 20,null =False,blank=False)
     productionyear = models.CharField(max_length = 20,null =False,blank=False)
     capacity = models.PositiveIntegerField(null =False,blank=False)
-    date = models.DateField(null =False,blank=False)
-    hourmove = models.PositiveIntegerField(null =False,blank=False)
     
     def __str__(self) -> str:
-        return f'{self.driver,self.route,self.codebus}'
+        return f'{self.driver,self.codebus}'
     
-    @property
-    def remindcapacity(self):
-        return self.capacity - 1 
+    
+
+    
+class BusRoute(models.Model):
+        route = models.ForeignKey(Route,on_delete=models.CASCADE,null =False,blank=False)
+        bus = models.ForeignKey(Bus,on_delete=models.CASCADE,null =False,blank=False)
+        date = models.DateField(null =False,blank=False)
+        hourmove = models.PositiveIntegerField(null =False,blank=False)
+        capacity = models.PositiveIntegerField(null =False,blank=False)
+
+        @property
+        def capacitybus(self):
+            self.capacity = self.bus.capacity
+            return self.capacity  
+        @property
+        def remindcapacity(self):
+            return self.capacity - 1 
         
-
-
+        
+        def __str__(self) -> str:
+            return f'{self.route,self.date,self.hourmove}'
+        
+        
+        
 class Ticket(models.Model):
     passenger = models.ForeignKey(Passenger,on_delete=models.CASCADE)
-    bus = models.ForeignKey(Bus,on_delete=models.CASCADE)
-    Cost = models.PositiveIntegerField()
+    busRoute = models.ForeignKey(BusRoute,on_delete=models.CASCADE)
+    cost = models.PositiveIntegerField()
     
     def __str__(self) -> str:
-        return f'{self.passenger,self.bus}'
+        return f'{self.passenger,self.busRoute}'
     
     @property
     def costticket(self):
-        self.cost = self.bus.route.distance * 1000
+        self.cost = self.busRoute.route.distance * 1000
         return self.cost
+    
+    
