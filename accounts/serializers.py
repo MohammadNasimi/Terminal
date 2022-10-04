4d34175d61582ae32885da23720e28e6a9918337
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-04 14:53:06 +0330

    add route serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,6 @@
 from ast import Pass
 from rest_framework import serializers
-from accounts.models import User,Driver,Passenger
+from accounts.models import User,Driver,Passenger,Manager
 class LoginSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
@@ -8,7 +8,13 @@ class LoginSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'phone', 'password','email', 'first_name', 'last_name','type')
         read_only_fields = ('id','email', 'first_name', 'last_name')
-        
+
+class RouteSerializer(serializers.ModelSerializer):
+    user = LoginSerializer()
+    class Meta:
+        model = Manager
+        fields = ('id', 'user')
+                
 class Driverserializer(serializers.ModelSerializer):
     user = LoginSerializer()
     class Meta:
