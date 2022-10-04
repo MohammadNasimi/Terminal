c9320738c45707dc0390e20bc2524067ae39fcf1
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-05 00:20:46 +0330

    read_only_fields add for BusRouteserializer

--- terminal/serializers.py
+++ terminal/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from terminal.models import Route,Bus,Ticket
+from terminal.models import BusRoute, Route,Bus,Ticket
 #ser accounts model 
 from accounts.serializers import ManagerSerializer,Driverserializer
 class Routeserializer(serializers.ModelSerializer):
@@ -17,11 +17,12 @@ class Busserializer(serializers.ModelSerializer):
         read_only_fields = ['id']
 
 class BusRouteserializer(serializers.ModelSerializer):
-    driver = Driverserializer(read_only =True)
+    bus = Busserializer(read_only =True)
+    routedata = Routeserializer( read_only =True)
     class Meta:
-        model = Bus
-        fields = ('id', 'route','bus','date','hourmove','capacity')
-        read_only_fields = ['id']
+        model = BusRoute
+        fields = ('id', 'route','bus','date','hourmove','capacity','routedata')
+        read_only_fields = ['id','capacity']
         
 
 class Ticketserializer(serializers.ModelSerializer):
