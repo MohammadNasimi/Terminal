430fa56747aba6a3a225ed82899805dd581e42ac
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-04 23:09:29 +0330

    permissions bus

--- terminal/permissions.py
+++ terminal/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-
+# permssions Route
 class IsOwnerOrReadOnlyRoute(permissions.BasePermission):
     
     def has_permission(self, request, view):
@@ -9,6 +9,25 @@ class IsOwnerOrReadOnlyRoute(permissions.BasePermission):
         
         if request.method == 'POST':
             if request.user.type == '1':
+                return True
+    
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `owner`.
+        return obj.owner == request.user
+    
+#permissions Bus
+class IsOwnerOrReadOnlyBus(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            if request.user.type == '1' or request.user.type == '2':
+                return True
+        
+        if request.method == 'POST':
+            if request.user.type == '2':
                 return True
     
     def has_object_permission(self, request, view, obj):
