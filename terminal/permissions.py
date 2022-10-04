de6873e73b6ab12eb7e3f2696c233ec1f8426fd8
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-05 00:21:02 +0330

    permissions BusRoute

--- terminal/permissions.py
+++ terminal/permissions.py
@@ -37,4 +37,21 @@ class IsOwnerOrReadOnlyBus(permissions.BasePermission):
 
         # Instance must have an attribute named `owner`.
         return obj.owner == request.user
+
+#permissions BusRoute
+class IsOwnerOrReadOnlyBusRoute(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+                return True
+        
+        if request.method == 'POST':
+            if request.user.type == '2':
+                return True
     
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `owner`.
+        return obj.owner == request.user
