fde8c30aae09fd8d7179695b1b6cf441eff1acf8
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-05 13:45:28 +0330

    add ticket url list

--- terminal/urls.py
+++ terminal/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
-from terminal.views import CreateRouteView,CreateBusView,CreateBusRouteView
+from terminal.views import CreateRouteView,CreateBusView,CreateBusRouteView,CreateTicketView
 urlpatterns = [
     path('route/list/', CreateRouteView.as_view(), name='list_route'),
     path('bus/list/', CreateBusView.as_view(), name='list_bus'),
     path('busroute/list/', CreateBusRouteView.as_view(), name='list_busroute'),
+    path('ticket/list/', CreateTicketView.as_view(), name='list_ticket'),
+
 
 ]
