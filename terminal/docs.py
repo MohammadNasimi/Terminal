0bbc085d645cf2df82f7099c5145802478ab12dd
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-07 15:13:08 +0330

    add route docs

--- terminal/docs.py
+++ terminal/docs.py
@@ -1,15 +1,41 @@
 #Route docs
 Route_list_get ="""
+ مدیر و راننده میتواند مسیر هاراببینند
+ sample:
+ {
+	"begin": "مبدا",
+	"destination": "مقصد",
+	"numberstation": تعداد ایستگاه,
+	"distance": فاصله بر اساس کیلومتر,
+	"timeroute": زمان رسیدن از میدا به مقصد به دقیقه 
+}
 """
 Route_list_post="""
+  فقط مدیر مسیر جدید ایجاد میکند
+ sample:
+ {
+	"begin": "مبدا",
+	"destination": "مقصد",
+	"numberstation": تعداد ایستگاه,
+	"distance": فاصله بر اساس کیلومتر,
+	"timeroute": زمان رسیدن از میدا به مقصد به دقیقه 
+}
 """
 Route_detail_retrieve ="""
+ مدیر و راننده میتواند مسیر هاراببینند
+
 """
 Route_detail_update="""
+سازنده راه فقط میتواند اپدیت کند
 """
+
 Route_detail_patch="""
+سازنده راه فقط میتواند اپدیت کند
+
 """
 Route_detail_destroy ="""
+سازنده راه فقط میتواند پاک کند
+
 """
 # Bus docs
 Bus_list_get ="""
