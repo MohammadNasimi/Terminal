34f21de221823bb1fcaea5ce43647e2edd24411c
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-08 13:45:22 +0330

    / end pk

--- terminal/urls.py
+++ terminal/urls.py
@@ -4,15 +4,15 @@ from terminal.views import CreateRouteView,CreateBusView,CreateBusRouteView,\
                     UpdateTicketView
 urlpatterns = [
     path('route/list/', CreateRouteView.as_view(), name='list_route'),
-    path('route/detail/<int:pk>', UpdateRouteView.as_view(), name='detail_route'),
+    path('route/detail/<int:pk>/', UpdateRouteView.as_view(), name='detail_route'),
 
     path('bus/list/', CreateBusView.as_view(), name='list_bus'),
-    path('bus/detail/<int:pk>', UpdateBusView.as_view(), name='detail_bus'),
+    path('bus/detail/<int:pk>/', UpdateBusView.as_view(), name='detail_bus'),
 
     path('busroute/list/', CreateBusRouteView.as_view(), name='list_busroute'),
-    path('busroute/detail/<int:pk>', UpdateBusRouteView.as_view(), name='detail_busroute'),
+    path('busroute/detail/<int:pk>/', UpdateBusRouteView.as_view(), name='detail_busroute'),
 
     path('ticket/list/', CreateTicketView.as_view(), name='list_ticket'),
-    path('ticket/detail/<int:pk>', UpdateTicketView.as_view(), name='detail_ticket'),
+    path('ticket/detail/<int:pk>/', UpdateTicketView.as_view(), name='detail_ticket'),
 
 ]
