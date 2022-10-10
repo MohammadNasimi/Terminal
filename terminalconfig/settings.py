0ddb758276348c2935dbba90b1befcad79ca9f8e
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-10 11:39:41 +0330

    add filter setting in djagno

--- terminalconfig/settings.py
+++ terminalconfig/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'django_filters',
 
     ]
 
@@ -137,8 +138,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
-    
+    'PAGE_SIZE': 5,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+
 }
 ##swagger
 SWAGGER_SETTINGS = {
