f22f326fd5e352445c70fc771861dfd5e76ead39
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-15 11:37:02 +0330

    add register passenger and driver

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
 from rest_framework_simplejwt import views as jwt_views
-from accounts.views import  LoginView, RegisterView
+from accounts.views import  LoginView, RegisterPassengerView,RegisterDriverView
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('login/refresh/', jwt_views.TokenRefreshView.as_view(), name='login_refresh'),
-    path('register/',RegisterView.as_view(),name='register')
+    path('register/passenger/',RegisterPassengerView.as_view(),name='register_passsenger'),
+    path('register/driver/',RegisterDriverView.as_view(),name='register_driver')
+
 ]
