f0f0842b4c0a87ab599446a434a81334e77d0bdd
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-10-15 11:36:41 +0330

    change register passenger and driver

--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,8 @@ from accounts.Validations import validate_phone,validate_password,validate_birth
 from rest_framework_simplejwt.tokens import RefreshToken
 #django auth 
 from django.contrib.auth import authenticate
+#docs
+from accounts import docs
 # log in
 class LoginView(generics.GenericAPIView):
     queryset = User.objects.all()
@@ -48,7 +50,7 @@ class LoginView(generics.GenericAPIView):
             return Response({'user':'wrong username or password'}, status=status.HTTP_200_OK)
         
 #register
-class RegisterView(APIView):
+class RegisterPassengerView(APIView):
     def post(self, request, *args, **kwargs):
         email= request.data.get("email" ,"")
         first_name= request.data.get("first_name" ,"")
@@ -57,12 +59,11 @@ class RegisterView(APIView):
         def create_user():
             user =User.objects.create_user(phone=request.data.get('phone'),password=request.data.get('password'),
                 email=email,first_name=first_name,
-                last_name=last_name,type = request.data.get('type'))
+                last_name=last_name,type = '3')
             return user
         
         
         if request.data.get('phone') == None or request.data.get('password') == None \
-                            or request.data.get('type') == None \
                             or request.data.get('password2') == None:
                 return Response({"detail": "اطلاعات ارسالی کامل نیست."} , status=status.HTTP_400_BAD_REQUEST)
         # validate     
@@ -86,16 +87,7 @@ class RegisterView(APIView):
                 return Response({'detail:':'age more than 15'},
                                 status=status.HTTP_400_BAD_REQUEST)
         #################################
-        # check type 
         try:
-            if request.data.get('type') == '2':
-                if not  request.data.get('Certificatenumber'):
-                    return Response({"detail"  : "Certificatenumber not exist"} , status=status.HTTP_400_BAD_REQUEST)
-                else:
-                    user =create_user()
-                    Certificatenumber = request.data.get('Certificatenumber')
-                    Driver.objects.create(user =user,Certificatenumber=Certificatenumber )
-            elif request.data.get('type') == '3':
                 if not  request.data.get('nationalcode') or not request.data.get('birthday'):
                     return Response({"detail"  : "nationalcode or birthday not exist"} , status=status.HTTP_400_BAD_REQUEST)
                 else:
@@ -103,13 +95,49 @@ class RegisterView(APIView):
                     birthday = request.data.get('birthday')
                     user = create_user()
                     Passenger.objects.create(user =user,nationalcode=nationalcode,
-                                            birthday=birthday,accountbalance = 0)
-            elif request.data.get('type') =='1':
-                user= create_user()
-            else :
-                return Response({"detail"  : "type should 1,2,3"} , status=status.HTTP_400_BAD_REQUEST)
-            
+                                            birthday=birthday,accountbalance = 0)            
         except:
             return Response({"detail"  : "username exist"} , status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response(request.data, status=status.HTTP_201_CREATED)
+##############################################
+############register driver ##################
+class RegisterDriverView(APIView):
+    def post(self, request, *args, **kwargs):
+        email= request.data.get("email" ,"")
+        first_name= request.data.get("first_name" ,"")
+        last_name= request.data.get("last_name" ,"")
+        # create-user
+        def create_user():
+            user =User.objects.create_user(phone=request.data.get('phone'),password=request.data.get('password'),
+                email=email,first_name=first_name,
+                last_name=last_name,type = '2')
+            return user
+        
+        
+        if request.data.get('phone') == None or request.data.get('password') == None \
+                            or request.data.get('password2') == None:
+                return Response({"detail": "اطلاعات ارسالی کامل نیست."} , status=status.HTTP_400_BAD_REQUEST)
+        # validate     
+        try:
+            validate_phone(request.data.get('phone'))
+        except:
+            return Response({'detail:':'you should start with 09 or size equal 11 example: 09141231213'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        try:
+            validate_password(request.data.get('password'),request.data.get('password2'))
+        except:
+            return Response({'detail:':'password must to mutch'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        #################################
+        try:
+                if not  request.data.get('Certificatenumber'):
+                    return Response({"detail"  : "Certificatenumber not exist"} , status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    user =create_user()
+                    Certificatenumber = request.data.get('Certificatenumber')
+                    Driver.objects.create(user =user,Certificatenumber=Certificatenumber )
+        except:
+                return Response({"detail"  : "username exist"} , status=status.HTTP_400_BAD_REQUEST)
         
         return Response(request.data, status=status.HTTP_201_CREATED)
